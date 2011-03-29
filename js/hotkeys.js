6f17262d664f53a99a33bc35cf25aa2e5503eb73
Qianlong Zhao <qianlongzhao@gmail.com>
2011-03-29 15:47:27 +0200

    petit modifications

--- js/hotkeys.js
+++ js/hotkeys.js
@@ -2,28 +2,28 @@
 * JS of Hotkeys Settings
 * Editor: Qianlong ZHAO
 *****************/
+function getKey(e){
+    var key;
+    if(window.event){        //IE
+        key=e.event;
+    }else if(e.which){        //netscape/firefox/opera
+        key=e.which;
+    }    
+    return key;
+}
 $(document).ready(function(){
-   $('#editor').keydown(function(e) {
-        var key;
-        if(window.event){        //IE
-            key=e.event;
-        }else if(e.which){        //netscape/firefox/opera
-            key=e.which;
-        }
-      //  alert(key);
+    $('#editor').keydown(function(e) {
+	var key;
+	key=getKey(e);
         if(key==18){
-	    $('#editor').keydown(function(e) {
+	    $('#editor').keydown(function(e1) {
 		var key2;
-		if(window.event){        //IE
-		    key2=e.event;
-		}else if(e.which){        //netscape/firefox/opera
-		    key2=e.which;
-		}
-	//	alert(key);
-		if(key2==73){
+		key2=getKey(e1);
+		if(key==18 && key2==73){
 		    $.markItUp({ openWith:'\\item ', closeWith:''});
+		    key=1;
 		}
 	    });	    
 	}
-   });
+    });
 });
